18b564aa2463a84e6e3d16683893eb0b3c5bc4bf
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2022-08-30 19:34:51 +0100

    add solution

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const list = document.querySelector(".gallery");
+const newImg = galleryItems
+  .map(
+    (image) =>
+      `<a class="gallery__item" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+        data-source="${image.original}"                      
+      alt="${image.description}"
+    />
+  </a>`
+  )
+  .join("");
+
+list.insertAdjacentHTML("beforeend", newImg);
+
+const lightbox = new SimpleLightbox(".gallery .gallery__item", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
+list.addEventListener("click", (e) => {
+  e.preventDefault();
+  lightbox.open();
+});
 
 console.log(galleryItems);
